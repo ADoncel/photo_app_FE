fc2d637e42f7e278a9f426f505db78edd8222f71
Antonio Doncel Campos <adoncel@users.noreply.github.com>
2023-10-27 15:18:45 +0200

    fix browser router

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import React from "react";
 import "./App.css";
 import Login from "./page/Login/Login";
 import Album from "./component/Album/Album";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [user, setUser] = React.useState<string>("user");
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/user" element={<Album />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/user" element={<Album />} />
+      </Routes>
     </div>
   );
 }
